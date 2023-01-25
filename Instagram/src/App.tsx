e1dea3cfbb33c3de1255f98ef292b5f83134e8f2
Shobika Senthilkumar <shobikasenthilkumar@CESs-MacBook-Pro.local>
2023-01-25 17:10:22 +0530

    Added initial setup

---
+++ Instagram/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import CreatePost from "./Components/CreatePost"
+
+function App() {
+  
+  return (
+    <>
+    <h1>Hello Shobika</h1>
+    <p>Welcome to One days</p>
+    <CreatePost/>
+    </>
+  )
+}
+
+export default App
